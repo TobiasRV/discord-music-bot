91bd7c33b4bb4919f25e49dfc96385c7a8c65016
Tobias Rodriguez Viau <rodriguezviautobias@gmail.com>
2023-12-23 15:54:33 -0300

    remake the play funcion

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,68 +1,18 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder } = require("discord.js");
-const { QueryType, useMainPlayer } = require("discord-player");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { useMainPlayer } = require('discord-player');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("Plays a song from YouTube.")
-    .addStringOption((option) =>
-      option.setName("url").setDescription("the song's url").setRequired(true)
-    ),
-  execute: async ({ client, interaction }) => {
-    try {
-      const url = interaction.options.getString("url").trim();
-      if(!url) return interaction.reply("Url required");
-      console.log('=====================================================================');
-      console.log('url', url);
-      console.log('=====================================================================');
-      if (!interaction.member.voice.channel)
-        return interaction.reply(
-          "You need to be in a Voice Channel to play a song."
-        );
-      const player = useMainPlayer();
+    .setName('play')
+    .setDescription('Plays a song from YouTube.')
+    .addStringOption((option) => option.setName('url').setDescription("the song's url").setRequired(true)),
+  execute: async ({ interaction }) => {
+    const url = interaction.options.getString('url');
 
-      const searchResult = await player.search(url, { requestedBy: interaction.user });
-      const { tracks } = searchResult;
-      console.log('=====================================================================');
-      console.log('searchResult', searchResult);
-      console.log('=====================================================================');
-      console.log('=====================================================================');
-      console.log('tracks', tracks);
-      console.log('=====================================================================');
-      if(!searchResult){
-        return interaction.reply("Song not found");
-      }
+    const player = useMainPlayer();
 
-      const trackResponse = await player.play(interaction.member.voice.channel, url, {
-        nodeOptions: {
-          metadata: interaction,
-        },
-      });
+    await player.play(interaction.member.voice.channel, url);
 
-      console.log('=========================trackResponse===============================');
-      console.log(JSON.stringify(trackResponse));
-      console.log('=====================================================================');
-
-      const { track } = trackResponse;
-
-      let embed = new EmbedBuilder();
-  
-      embed
-        .setDescription(
-          `**[${track.title}](${track.url})** has been added to the Queue.`
-        )
-        .setThumbnail(track.thumbnail)
-        .setFooter({ text: `Duration: ${track.duration}` });
-  
-      await interaction.reply({
-        embeds: [embed],
-      });
-    } catch (error) {
-      console.log('=========================ERROR========================================');
-      console.log(JSON.stringify(error));
-      console.log('=====================================================================');
-      return interaction.reply(`error playing song, url: ${url}`);
-    }
+    await interaction.reply('Playing...');
   },
 };
